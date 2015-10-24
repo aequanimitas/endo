a6570c1ae5d13217cc4de9593f9fac2de6c85018
metalfatigue <htavenido@gmail.com>
2015-10-24 15:36:07 +0800

    just copy the function on how tedious handles arguments

--- index.js
+++ index.js
@@ -62,4 +62,15 @@ function init(args) {
   }
 }
 
+function startTask(opts) {
+  console.log("\nTedious: For things that you do repeatedly\n");
+  if (opts.length == 0) {
+    console.log("Available commands: " + Object.keys(subapps).join(", ") + "\n");
+  } else if (opts.length == 1) {
+    console.log("Available commands for " + opts[0] + ": " + subapps[opts[0]].operations.join(", ") + "\n");
+  } else {
+    subapps[opts[0]].init(opts.slice(1));
+  }
+}
+
 init(process.argv.slice(2));
