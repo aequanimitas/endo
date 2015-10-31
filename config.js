37bb8096b9bfa5b852ec8b336329b9e7efeecf86
metalfatigue <htavenido@gmail.com>
2015-10-31 15:14:06 +0800

    Cleanup
    - remove db config, use knex-suggested setup
    - add simple cli

--- config.js
+++
@@ -1,7 +0,0 @@
-var exports = module.exports = {
-  vendor: "postgres",
-  username: "user",
-  password: "password",
-  url: "localhost",
-  db_name: "db_name"
-};
---
+++ tresdin.js
@@ -0,0 +1,43 @@
+var appName = "", appDescription = "";
+exports = module.exports = {};
+
+var messages = {
+  unOpts: function(m) { return "Unrecognized option: " + m + "\n"; }
+};
+
+function circular(app, opt, args) {
+  if (opOrApp(app, opt, "subapps")) {
+    circular(app.subapps[opt], args[0], args.slice(1));
+  } else if (opOrApp(app, opt, "operations")) {
+    app.operations[opt]();
+  } else {
+//    process.stdout.write("Unrecognized option: " + opt);
+    help(app);
+  }
+}
+
+exports.sequential = function(app) {
+  if (app.args.length == 0) {
+    app.hasOwnProperty("help") ? app.help() : help(app);
+    return;
+  }
+  appName = app.name;
+  appDescription = app.description;
+  circular(app, app.args[0], app.args.slice(1));
+}
+
+function opOrApp(app, opt, prop) {
+  return !(app[prop] === undefined) && app[prop].hasOwnProperty(opt);
+}
+
+function help(app) {
+  var keyz = Object.keys(app.subapps ? app.subapps : app.operations).join(", ");
+  if (keyz.length == 0) keyz = "none"
+  process.stdout.write("\n" + appName + ": " + appDescription + "\n\n");
+  process.stdout.write("\Usage:");
+  keyz.split(", ").forEach(function(v) {
+    var appInvoke = appName + " " + app.name;
+    process.stdout.write("\n  " + appInvoke + " " + v);
+  });
+  process.stdout.write("\n");
+}
