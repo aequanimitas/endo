ab14d44ccaf88fcac19fd15dfe4112b0e73a0510
aequanimitas <htavenido@gmail.com>
2015-11-04 04:54:02 +0800

    cleanup

--- cli.js
+++ cli.js
@@ -1,43 +1,4 @@
 exports = module.exports = {};
-var appName = "", 
-    appDescription = "",
-    messages = require('./error').messages;
-
-function circular(app, opt, args) {
-  if (opOrApp(app, opt, "subapps")) {
-    circular(app.subapps[opt], args[0], args.slice(1));
-  } else if (opOrApp(app, opt, "operations")) {
-    app.operations[opt]();
-  } else {
-    help(app);
-  }
-}
-
-exports.sequential = function(app) {
-  if (app.args.length == 0) {
-    app.hasOwnProperty("help") ? app.help() : help(app);
-    return;
-  }
-  appName = app.name;
-  appDescription = app.description;
-  circular(app, app.args[0], app.args.slice(1));
-}
-
-function opOrApp(app, opt, prop) {
-  return !(app[prop] === undefined) && app[prop].hasOwnProperty(opt);
-}
-
-function help(app) {
-  var keyz = Object.keys(app.subapps ? app.subapps : app.operations).join(", "),
-      message = "\n" + appName + ": " + appDescription + "\n\n" +
-                "\nUsage";
-  if (keyz.length == 0) keyz = "none"
-  keyz.split(", ").forEach(function(v) {
-    var appInvoke = appName + " " + app.name;
-    message += "\n  " + appInvoke + " " + v;
-  });
-  exitMessage(message);
-}
 
 function arrDiff(x, y) {
   return x.filter(function(a) {
@@ -48,6 +9,19 @@ function arrDiff(x, y) {
 function flagsHasArguments(x) {
   var t = x.split('=');
   return (t[1] === undefined || t[1] === '')
+}
+
+function partial(aFn, aArgFn) {
+  return function inFn(b) {
+    return aFn(b, aArgFn);
+  }
+}
+
+function toKv(x, cb) {
+  var y = x.split('=');
+  if (cb) y[0] = cb(y[0])
+  y[0] = y[1];
+  return y;
 }
 
 function toObjPair(pairs, cb) {
