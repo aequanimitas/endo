dd0c86d70f074bd6ff07f2b5962f43ddf36ca950
aequanimitas <htavenido@gmail.com>
2015-12-30 05:40:23 +0800

    ugly, but it works

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 var pkg = require('./package.json');
 var appInfo = `${pkg.name} v${pkg.version}`;
-var message = appInfo + '\nUsage:';
 
 function start() {
   var args = process.argv;
-  if (args.slice(2).length === 0) {
+  args = args.slice(2);
+  var dict = args.map(function(o) {
+    var obj = {}, 
+        temp = o.split('=');
+    obj[temp[0]] = temp[1];
+    return obj;
+  });
+  if (!args.length || !dict.length) {
+    var usage = [];
+    usage.push('    -i, --item    Item name');
+    usage.push('    -e, --expiry  Expiration Date');
+    var message = appInfo + '\nUsage:\n' + usage.join('\n');
     console.log(message);
+    process.exit(1);
+  } else {
+    dict.forEach(function(e,i,a) {
+      if (Object.keys(e).indexOf('--item') !== -1 ||
+          Object.keys(e).indexOf('--expiry') !== -1) {
+        if (e[Object.keys(e)[0]] !== undefined) {
+          console.log('Hi');
+        } else {
+          var message = appInfo  + '\n-i, --item argument has no value';
+          console.log(message);
+          process.exit(1);
+        }
+      }
+    });
   }
 }
 
--- test/bats/index.bats
+++ test/bats/index.bats
@@ -4,4 +4,12 @@
         run node index.js
         [ "${lines[0]}" = "Endo v0.0.1" ]
         [ "${lines[1]}" = "Usage:" ]
+        [ "${lines[2]}" = '    -i, --item    Item name' ]
+        [ "${lines[3]}" = '    -e, --expiry  Expiration Date' ]
+}
+
+@test "Called --item with no value given" {
+        run node index.js --item
+        [ "${lines[0]}" = "Endo v0.0.1" ]
+        [ "${lines[1]}" = '-i, --item argument has no value' ]
 }
